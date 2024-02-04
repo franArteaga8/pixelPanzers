de7ede6af0075fd5a560f6e09aa8ae1f9ae3febb
Fran Arteaga <fran.am8@gmail.com>
2024-02-04 19:45:04 +0000

    created class Bullets, created function spawnBullets()

---
+++ src/bullets.js
@@ -0,0 +1,33 @@
+class Bullets {
+    constructor (x, y, parent, enemies, obstacles, bullets) {
+        this.x = x
+        this.y = y
+        this.parent = parent
+        this.enemies = enemies
+        this.obstacles = obstacles
+        this.bullets = bullets
+        this.speed = 5
+        this.width = 5
+        this.height = 5
+        this.sprite
+
+    }
+
+    spawnBullets () {
+        let newBullet = document.createElement('div')
+        newBullet.classList.add('bullet')
+        newBullet.style.top = this.y + 'px'
+        newBullet.style.left = this.x + 'px'
+        this.parent.appendChild(newBullet)
+        this.sprite = newBullet
+
+    }
+
+    move() {
+
+    }
+
+    despawnBullets () {
+
+    }
+}
